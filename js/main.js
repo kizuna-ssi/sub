f51df42c1fcf41afab93dee7e7c849eeb1fb5f95
kizuna-ssi <161543253+kizuna-ssi@users.noreply.github.com>
2024-03-11 09:41:53 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -105,10 +105,11 @@ $(function() {
 
 
 
-      fetch("header.html")
-        .then((response) => response.text())
-        .then((data) => document.querySelector("#header").innerHTML = data);
-
-      fetch("footer.html")
-        .then((response) => response.text())
-        .then((data) => document.querySelector("#footer").innerHTML = data);
+      $(function() {
+        $.get('header.html', function(data){
+          $('body').prepend(data);
+        });
+        $.get('footer.html', function(data){
+          $('body').append(data);
+        });
+      });
