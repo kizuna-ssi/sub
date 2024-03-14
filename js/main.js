4ba1b08859a0be49380f04f4f9a4bb93c2e78637
kizuna-ssi <161543253+kizuna-ssi@users.noreply.github.com>
2024-03-14 15:58:51 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -114,3 +114,13 @@ $(function(){
         });
 
 
+//　モーダル
+$(function() {
+  $('#exampleModal').on('show.bs.modal', function () {
+    var title = $('#formTitle').val()
+    var body = $('#formBody').val()
+    var modal = $(this)
+    modal.find('#modalTitle').text(title)
+    modal.find('#modalBody').text(body)
+  })
+})
