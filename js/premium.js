2cbec6a7b1b8d6edd95e016dd40b5442a74b951e
kizuna-ssi <161543253+kizuna-ssi@users.noreply.github.com>
2024-03-28 09:47:53 +0900

    Update premium.js

--- js/premium.js
+++ js/premium.js
@@ -2411,3 +2411,35 @@
       document.getElementById("product").innerText = "引受不可";
     }
   }
+
+function display() {
+    // 生年月日の取得
+    var year = document.querySelector('.birthday-year').value;
+    var month = document.querySelector('.birthday-month').value;
+    var day = document.querySelector('.birthday-day').value;
+
+    // 性別の取得
+    var gender;
+    if (document.getElementById('male').checked) {
+        gender = '男性';
+    } else if (document.getElementById('female').checked) {
+        gender = '女性';
+    }
+
+    // 保険金額の取得
+    var amount;
+    if (document.getElementById('50').checked) {
+        amount = '50万円';
+    } else if (document.getElementById('100').checked) {
+        amount = '100万円';
+    } else if (document.getElementById('200').checked) {
+        amount = '200万円';
+    } else if (document.getElementById('300').checked) {
+        amount = '300万円';
+    }
+
+    // 生年月日、性別、保険金額を表示
+    document.getElementById('birthdayLabel').innerText = '生年月日: ' + year + '年' + month + '月' + day + '日';
+    document.getElementById('genderLabel').innerText = '性別: ' + gender;
+    document.getElementById('amountLabel').innerText = '保険金額: ' + amount;
+}
