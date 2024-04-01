ca1cf0a406710ce4ab1d7994fa7143e37062e522
kizuna-ssi <161543253+kizuna-ssi@users.noreply.github.com>
2024-04-01 12:12:25 +0900

    Update recruitcode.js

--- js/recruitcode.js
+++ js/recruitcode.js
@@ -11,7 +11,10 @@ $(document).ready(function(){
           var found = false;
           for (var i = 0; i < lines.length; i++) {
             var parts = lines[i].split('","');
-
+            // ダブルクォーテーションを削除
+            parts = parts.map(function(part) {
+              return part.replace(/^"(.*)"$/, '$1');
+            });
             if (parts[0] === recruitCode) {
               $('input[name="募集人名"]').val(parts[1]);
               $('input[name="代理店名"]').val(parts[3]);
